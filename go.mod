4e13c70280532a054560ce7e4d669c9b7d2dfd92
nguyenmanhthao996tn <manhthao.spm@gmail.com>
2024-05-02 01:30:03 +0200

    Update: Add TOML configuration file

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
--- mqtt.go
+++ mqtt.go
@@ -9,16 +9,31 @@ import (
     "strings"
     "github.com/chirpstack/chirpstack/api/go/v4/gw"
     "encoding/json"
+	"os"
+	"github.com/BurntSushi/toml"
 )
 
+// ==================================
+
+type Config struct {
+	Mqtt_broker_address 		string
+	Mqtt_broker_port 				int
+	Mqtt_client_id 					string
+	Mqtt_client_username 		string
+	Mqtt_client_password 		string
+	Mqtt_listen_topic 			string
+	Mqtt_translate_topic 		string
+}
+var conf Config
+
+// ==================================
+
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-    // fmt.Printf("Received message: %v from topic: %s\n", msg.Payload(), msg.Topic())
 
     split_topic_arr := strings.Split(string(msg.Topic()), "/")
-    // fmt.Printf("split_topic_arr: ", split_topic_arr)
     if ((split_topic_arr[3] == "event") && (split_topic_arr[4] == "up")) {
         fmt.Println("====================");
-        fmt.Printf("Topic: event/up\n");
+        fmt.Println("Topic: event/up");
 
         gw_uplink := &gw.UplinkFrame{}
         err := proto.Unmarshal(msg.Payload(), gw_uplink)
@@ -35,7 +50,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 
     } else if ((split_topic_arr[3] == "event") && (split_topic_arr[4] == "stats")) {
         fmt.Println("====================");
-        fmt.Printf("Topic: event/stats\n");
+        fmt.Println("Topic: event/stats");
 
         gw_data := &gw.GatewayStats{}
         err := proto.Unmarshal(msg.Payload(), gw_data)
@@ -52,7 +67,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 
     } else if ((split_topic_arr[3] == "event") && (split_topic_arr[4] == "ack")) {
         fmt.Println("====================");
-        fmt.Printf("Topic: event/ack\n");
+        fmt.Println("Topic: event/ack");
 
         gw_data := &gw.DownlinkTxAck{}
         err := proto.Unmarshal(msg.Payload(), gw_data)
@@ -69,7 +84,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 
     } else if ((split_topic_arr[3] == "event") && (split_topic_arr[4] == "exec")) {
         fmt.Println("====================");
-        fmt.Printf("Topic: event/exec\n");
+        fmt.Println("Topic: event/exec");
 
         gw_data := &gw.GatewayCommandExecResponse{}
         err := proto.Unmarshal(msg.Payload(), gw_data)
@@ -86,7 +101,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 
         } else if ((split_topic_arr[3] == "event") && (split_topic_arr[4] == "raw")) {
             fmt.Println("====================");
-            fmt.Printf("Topic: event/raw\n");
+            fmt.Println("Topic: event/raw");
     
             gw_data := &gw.RawPacketForwarderEvent{}
             err := proto.Unmarshal(msg.Payload(), gw_data)
@@ -103,7 +118,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 
             } else if ((split_topic_arr[3] == "command") && (split_topic_arr[4] == "down")) {
                 fmt.Println("====================");
-                fmt.Printf("Topic: command/down\n");
+                fmt.Println("Topic: command/down");
         
                 gw_data := &gw.DownlinkFrame{}
                 err := proto.Unmarshal(msg.Payload(), gw_data)
@@ -119,7 +134,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
                 fmt.Println("====================");
         
     } else {
-        fmt.Printf("Message type / topic handler is not defined yet, topic: %s\n", msg.Topic());
+        fmt.Println("Message type/topic handler is not defined yet, topic: ", msg.Topic());
     }
 }
 
@@ -128,17 +143,20 @@ var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
 }
 
 var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-    fmt.Printf("Connect lost: %v", err)
+    fmt.Println("Connect lost: ", err)
 }
 
 func main() {
-    var broker = "manhthao.ovh"
-    var port = 11883
+    set_conf_default_value(&conf)
+    read_conf_from_toml(&conf)
+
+    var broker = conf.Mqtt_broker_address
+    var port = conf.Mqtt_broker_port
     opts := mqtt.NewClientOptions()
     opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
-    opts.SetClientID("go_mqtt_client")
-    opts.SetUsername("emqx")
-    opts.SetPassword("public")
+    opts.SetClientID(conf.Mqtt_client_id)
+    opts.SetUsername(conf.Mqtt_client_username)
+    opts.SetPassword(conf.Mqtt_client_password)
     opts.SetDefaultPublishHandler(messagePubHandler)
     opts.OnConnect = connectHandler
     opts.OnConnectionLost = connectLostHandler
@@ -166,7 +184,8 @@ func publish(client mqtt.Client) {
 }
 
 func send_translated_message(client mqtt.Client, topic string, text string) {
-    translated_topic := strings.Join([]string{"translated", topic}, "/")
+    translated_topic := strings.Join([]string{conf.Mqtt_translate_topic, topic}, "/")
+    translated_topic = strings.ReplaceAll(translated_topic, "//", "/")
     fmt.Println("Sending translated JSON data to ", translated_topic);
 
     token := client.Publish(translated_topic, 0, false, text)
@@ -174,8 +193,43 @@ func send_translated_message(client mqtt.Client, topic string, text string) {
 }
 
 func sub(client mqtt.Client) {
-    topic := "as923_2/gateway/7276ff000f061f16/#"
+    listen_topic := conf.Mqtt_listen_topic
+    listen_topic = strings.ReplaceAll(listen_topic, "//", "/")
+
+    topic := listen_topic
     token := client.Subscribe(topic, 1, nil)
     token.Wait()
     fmt.Println("Subscribed to topic: %s", topic)
+}
+
+func set_conf_default_value(conf *Config) {
+	conf.Mqtt_broker_address = "manhthao.ovh"
+	conf.Mqtt_broker_port = 11883
+	conf.Mqtt_client_id = "client_id"
+	conf.Mqtt_client_username = ""
+	conf.Mqtt_client_password = ""
+	conf.Mqtt_listen_topic = "as923_2/gateway/7276ff000f061f16/"
+	conf.Mqtt_translate_topic = "translated/"
+}
+
+func read_conf_from_toml(conf *Config) {
+    dat, err := os.ReadFile("./config.toml")
+	check(err)
+
+    _, err1 := toml.Decode(string(dat), &conf)
+	check(err1)
+
+    fmt.Println("Mqtt_broker_address: ", conf.Mqtt_broker_address)
+	fmt.Println("Mqtt_broker_port: ", conf.Mqtt_broker_port)
+	fmt.Println("Mqtt_client_id: ", conf.Mqtt_client_id)
+	fmt.Println("Mqtt_client_username: ", conf.Mqtt_client_username)
+	fmt.Println("Mqtt_client_password: ", conf.Mqtt_client_password)
+	fmt.Println("Mqtt_listen_topic: ", conf.Mqtt_listen_topic)
+	fmt.Println("Mqtt_translate_topic: ", conf.Mqtt_translate_topic)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
